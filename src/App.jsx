4fa23c2c1a272de667f0a67e2d36f21bf7d1d80c
Subhiksha-G <subhikshavkl@gmail.com>
2026-05-24 12:09:05 +0530

    Initial commit: Set up React with custom Navbar component

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import Navbar from "./components/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="app-container">
+      <Navbar logoTitle="SUBHIKSHA G 🚀" />
+      <h1 style={{color: 'gold', textAlign: 'center', marginTop: '50px'}}>Subhiksha's React Portfolio Startup 🚀</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import './Navbar.css';
+
+function Navbar(props) {
+    return(
+        <nav className="react-navbar">
+            <div className='react-logo'>{props.logoTitle}</div>
+            <ul className='react-navlinks'>
+                <li><a href="#about">About</a></li>
+                <li><a href="#projects">Projects</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
