b628700e5378fab8523a82933b0c1cb8c980be94
Subhiksha-G <subhikshavkl@gmail.com>
2026-05-25 16:20:26 +0530

    Connected Navbar menu items to pages using React Router Links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,30 @@
+import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 function Navbar(props) {
     return(
         <nav className="react-navbar">
-            <div className='react-logo'>{props.logoTitle}</div>
+            <div className='react-logo'>
+                <Link to="/" style={{color: 'inherit', textDecoration: 'none'}}>
+                    {props.logoTitle}
+                </Link>
+            </div>
             <ul className='react-navlinks'>
-                <li><a href="#about">About</a></li>
-                <li><a href="#projects">Projects</a></li>
-                <li><span className='nav-badge'>Likes : {props.countValue} ❤️</span></li>
-                <li><a href="#contact">Contact</a></li>
+                <li>
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/projects">Projects</Link>
+                </li>
+                <li>
+                    <Link to="/contact">Contact</Link>
+                </li>
+                <li>
+                    <span className='nav-badge'>Likes : {props.countValue} ❤️</span>
+                </li>
             </ul>
         </nav>
     )
 }
 
-export default Navbar
+export default Navbar;
