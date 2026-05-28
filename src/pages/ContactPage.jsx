5db1cfb6034fd338e6ef723a1014bc417e7b43ac
Subhiksha-G <subhikshavkl@gmail.com>
2026-05-28 12:16:18 +0530

    Removed textAlign: 'center' CSS property in ContactPage.jsx file

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2,7 +2,7 @@ import ContactForm from "../components/ContactForm/ContactForm";
 
 function ContactPage() {
     return(
-        <div style={{padding: '20px', marginTop: '40px', textAlign: "center"}}>
+        <div style={{padding: '20px', marginTop: '40px'}}>
             <ContactForm />
         </div>
     );
